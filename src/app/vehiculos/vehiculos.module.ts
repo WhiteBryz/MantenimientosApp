a2c303e80a1baec65b7f0808eeca4558c16c553e
WhiteBryz <davidflores@live.com.mx>
2024-11-04 19:38:58 -0600

    Inicio del login

--- src/app/vehiculos/vehiculos.module.ts
+++ src/app/vehiculos/vehiculos.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { VehiculosPageRoutingModule } from './vehiculos-routing.module';
 
 import { VehiculosPage } from './vehiculos.page';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    VehiculosPageRoutingModule
+    VehiculosPageRoutingModule,
+    SharedModule
   ],
   declarations: [VehiculosPage]
 })
-export class VehiculosPageModule {}
+export class VehiculosPageModule { }
