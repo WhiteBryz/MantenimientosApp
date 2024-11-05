e3d14cc265dd16e0088ad3b44a63d0d153704adc
David Aranda <dayaramon64@gmail.com>
2024-11-04 23:19:21 -0600

    Refactor task management UI

--- src/app/hogar/hogar.page.ts
+++ src/app/hogar/hogar.page.ts
@@ -15,6 +15,7 @@ export class HogarPage {
 
   mostrarBoton: boolean = true;
   actualizarBoton: boolean = false;
+  isModalOpen: boolean = false;
 
   tareas: {
     id: number;
@@ -23,9 +24,10 @@ export class HogarPage {
     frecuencia: string;
     estado: string;
     notas: string;
-    archivos: string[]; }[] = [];
+    archivos: string[];
+  }[] = [];
 
-    posicion: number = 0;
+  posicion: number = 0;
 
   constructor() {
     // Obtener tareas del localStorage
@@ -33,16 +35,23 @@ export class HogarPage {
     if (tareasLocal) {
       this.tareas = JSON.parse(tareasLocal);
     }
-    this.mostrarBoton = true;
-    this.actualizarBoton = false;
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.resetForm();
   }
 
   Eliminar(i: number) {
-    if (confirm("¿Deseas eliminar esta tarea?")) {
-    this.tareas.splice(i, 1);
-    localStorage.setItem('tareas', JSON.stringify(this.tareas));
+    if (confirm('¿Deseas eliminar esta tarea?')) {
+      this.tareas.splice(i, 1);
+      localStorage.setItem('tareas', JSON.stringify(this.tareas));
+    }
   }
-}
 
   Agregar() {
     if (this.valorDescripcion && this.valorFechaHora && this.valorFrecuencia && this.valorEstado) {
@@ -53,94 +62,71 @@ export class HogarPage {
         frecuencia: this.valorFrecuencia,
         estado: this.valorEstado,
         notas: this.valorNotas,
-        archivos: this.archivos
+        archivos: this.archivos,
       });
 
-      // Guardar tareas en localStorage
       localStorage.setItem('tareas', JSON.stringify(this.tareas));
-
-      alert('Tarea agregada con éxito');
-
-      // Limpiar campos después de agregar la tarea
-      this.valorDescripcion = '';
-      this.valorFechaHora = '';
-      this.valorFrecuencia = '';
-      this.valorEstado = '';
-      this.valorNotas = '';
-      this.archivos = [];
-    }
-    else {
+      this.closeModal();
+    } else {
       alert('Por favor, completa los campos requeridos');
     }
   }
 
-  Actualizar(){
+  Actualizar() {
     if (this.valorDescripcion && this.valorFechaHora && this.valorFrecuencia && this.valorEstado) {
-      this.tareas[this.posicion].descripcion = this.valorDescripcion;
-      this.tareas[this.posicion].fechaHora = this.valorFechaHora;
-      this.tareas[this.posicion].frecuencia = this.valorFrecuencia;
-      this.tareas[this.posicion].estado = this.valorEstado;
-      this.tareas[this.posicion].notas = this.valorNotas;
-      this.tareas[this.posicion].archivos = this.archivos;
+      this.tareas[this.posicion] = {
+        id: this.tareas[this.posicion].id,
+        descripcion: this.valorDescripcion,
+        fechaHora: this.valorFechaHora,
+        frecuencia: this.valorFrecuencia,
+        estado: this.valorEstado,
+        notas: this.valorNotas,
+        archivos: this.archivos,
+      };
 
-      this.mostrarBoton = true;
-      this.actualizarBoton = false;
-
-      // Guardar tareas en localStorage
       localStorage.setItem('tareas', JSON.stringify(this.tareas));
-
-      alert('Tarea actualizada con éxito');
-
-      // Limpiar campos después de agregar la tarea
-      this.valorDescripcion = '';
-      this.valorFechaHora = '';
-      this.valorFrecuencia = '';
-      this.valorEstado = '';
-      this.valorNotas = '';
-      this.archivos = [];
-      this.mostrarBoton = true;
-      this.actualizarBoton = false;
+      this.closeModal();
     }
   }
 
   Cancelar() {
-    this.mostrarBoton = true;
-    this.actualizarBoton = false;
-
-    this.valorDescripcion = '';
-    this.valorFechaHora = '';
-    this.valorFrecuencia = '';
-    this.valorEstado = '';
-    this.valorNotas = '';
-    this.archivos = [];
+    this.closeModal();
   }
 
   Editar(i: number) {
-    this.mostrarBoton = false;
-    this.actualizarBoton = true;
-
     this.posicion = i;
-
     this.valorDescripcion = this.tareas[i].descripcion;
     this.valorFechaHora = this.tareas[i].fechaHora;
     this.valorFrecuencia = this.tareas[i].frecuencia;
     this.valorEstado = this.tareas[i].estado;
     this.valorNotas = this.tareas[i].notas;
     this.archivos = this.tareas[i].archivos;
-    }
 
+    this.mostrarBoton = false;
+    this.actualizarBoton = true;
+    this.openModal();
+  }
 
-    // Método para almacenar y mostrar un archivo
-    onFileSelected(event: any) {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          const base64String = reader.result as string;
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const base64String = reader.result as string;
         this.archivos.push(base64String);
-        
-        };
-        reader.readAsDataURL(file);
-      }
-   }
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  private resetForm() {
+    this.valorDescripcion = '';
+    this.valorFechaHora = '';
+    this.valorFrecuencia = '';
+    this.valorEstado = '';
+    this.valorNotas = '';
+    this.archivos = [];
+    this.mostrarBoton = true;
+    this.actualizarBoton = false;
+  }
 }
