1b551b07f417d480925668d3aea65ac08e2bbab2
Nataly Iglesias <figlesias@ucol.mx>
2024-11-06 19:10:58 -0600

    Registro citas veterinario completo

--- src/app/citas-vet/citas-vet.module.ts
+++ src/app/citas-vet/citas-vet.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { CitasVetPageRoutingModule } from './citas-vet-routing.module';
 
 import { CitasVetPage } from './citas-vet.page';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CitasVetPageRoutingModule
+    CitasVetPageRoutingModule,
+    SharedModule
   ],
   declarations: [CitasVetPage]
 })
--- src/app/citas-vet/citas-vet.page.ts
+++ src/app/citas-vet/citas-vet.page.ts
@@ -1,15 +1,120 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-citas-vet',
   templateUrl: './citas-vet.page.html',
   styleUrls: ['./citas-vet.page.scss'],
 })
-export class CitasVetPage implements OnInit {
+export class CitasVetPage{
+  valorNombreMasc = '';
+  valorFecha = '';
+  valorMotivo = '';
+  valorResultado = '';
+  valorNotas = '';
+  valorAlerta: boolean = false;
 
-  constructor() { }
+  mostrarBoton: boolean = true;
+  actualizarBoton: boolean = false;
+  isModalOpen: boolean = false;
 
-  ngOnInit() {
+  citas:{
+    id: number;
+    idMascota: number,
+    nombreMasc: string,
+    fecha: string,
+    motivo: string,
+    resultado: string,
+    notas: string,
+    alerta: boolean
+  }[] = [];
+
+  posicion: number = 0;
+
+  constructor() {
+    // Obtener citas del localStorage
+    let citasLocal = localStorage.getItem('citas');
+    if (citasLocal) {
+      this.citas = JSON.parse(citasLocal);
+    }
+   }
+
+  openModal() {
+    this.isModalOpen = true;
   }
 
+  closeModal() {
+    this.isModalOpen = false;
+    this.resetForm();
+  }
+
+  Eliminar(i: number) {
+    if (confirm('¿Estás seguro de eliminar esta cita?')){
+    this.citas.splice(i, 1);
+    localStorage.setItem('citas', JSON.stringify(this.citas));
+    }
+  }
+
+  Agregar(){
+    if (this.valorNombreMasc && this.valorFecha && this.valorMotivo && this.valorResultado && this.valorAlerta) {
+      this.citas.push({
+        id: Date.now(),
+        idMascota: this.posicion,
+        nombreMasc: this.valorNombreMasc,
+        fecha: this.valorFecha,
+        motivo: this.valorMotivo,
+        resultado: this.valorResultado,
+        notas: this.valorNotas || 'Sin notas',
+        alerta: this.valorAlerta
+      });
+      localStorage.setItem('citas', JSON.stringify(this.citas));
+      this.closeModal();
+    }
+    else{
+      alert('Por favor, completa los campos requeridos');
+    }
+  }
+
+  Actualizar(){
+    if(this.valorNombreMasc && this.valorFecha && this.valorMotivo && this.valorResultado && this.valorAlerta !== null){
+      this.citas[this.posicion] = {
+        id: this.citas[this.posicion].id,
+        idMascota: this.posicion,
+        nombreMasc: this.valorNombreMasc,
+        fecha: this.valorFecha,
+        motivo: this.valorMotivo,
+        resultado: this.valorResultado,
+        notas: this.valorNotas || 'Sin notas',
+        alerta: this.valorAlerta
+      };
+      localStorage.setItem('citas', JSON.stringify(this.citas));
+      this.closeModal();
+    }
+  }
+
+  Cancelar(){
+    this.closeModal();
+  }
+
+  Editar(i: number){
+    this.posicion = i;
+    this.valorNombreMasc = this.citas[i].nombreMasc;
+    this.valorFecha = this.citas[i].fecha;
+    this.valorMotivo = this.citas[i].motivo;
+    this.valorResultado = this.citas[i].resultado;
+    this.valorNotas = this.citas[i].notas;
+    this.valorAlerta = this.citas[i].alerta;
+
+    this.mostrarBoton = false;
+    this.actualizarBoton = true;
+    this.openModal();
+  }
+
+  private resetForm(){
+    this.valorNombreMasc = '';
+    this.valorFecha = '';
+    this.valorMotivo = '';
+    this.valorResultado = '';
+    this.valorNotas = '';
+    this.valorAlerta = false;
+  }
 }
